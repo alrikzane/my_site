b813c206d2adbc4f3e100ee511ad393b24929106
alrikzane <ckill071@gmail.com>
2022-07-24 15:59:25 +0300

    created db tables Author, Tag, Post and superuser

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Author, Tag, Post
 
-# Register your models here.
+
+
+
+
+
+admin.site.register(Author)
+admin.site.register(Tag)
+admin.site.register(Post)
--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+import email
+from tkinter import CASCADE
+from django.db import DatabaseError, models
+from django.forms import CharField, DateField
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=35)
+    e_mail = models.CharField(max_length=50)
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=30)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="posts")
+    tag = models.ManyToManyField(Tag)
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=300)
+    image_link = models.CharField(max_length=300)
+    image_name = models.CharField(max_length=150)
+    date = models.DateField()
+    slug = models.SlugField()
+    content = models.CharField(max_length=2000)
+
+
+
+
+
+
