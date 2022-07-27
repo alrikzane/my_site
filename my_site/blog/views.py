e3d6d39cac5edae516e645736215fd777ee82198
alrikzane <ckill071@gmail.com>
2022-07-27 17:14:14 +0300

    change hardcoded date to DB data

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,144 +1,19 @@
 from datetime import date
+from operator import pos
 
 
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Tag, Author
+all_posts = Post.objects.all().order_by("-date")
+latest_posts = all_posts[:3]
 
-postlist = [
-    {
-        'slug' : 'mountain-hike',
-        'image' : 'mountains.jpg',
-        'author' : "Алексей",
-        'date' : date(2022, 7, 12),
-        'title' : 'mountain hiking',
-        'excerpt' : 'Нет ничего похожего на пейзажи, которые можно увидеть путешествуя по горам! Я не был готов к тому, что случилось, пока я наслаждался прекрасным видом...',
-        'content' : """
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias deleniti, error 
-            ipsum dolorem tempora voluptas repellendus eum corrupti aperiam sit suscipit voluptatem doloremque, 
-            tenetur ipsam dicta nobis quos non porro!
 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias deleniti, error 
-            ipsum dolorem tempora voluptas repellendus eum corrupti aperiam sit suscipit voluptatem doloremque, 
-            tenetur ipsam dicta nobis quos non porro!
-
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias deleniti, error 
-            ipsum dolorem tempora voluptas repellendus eum corrupti aperiam sit suscipit voluptatem doloremque, 
-            tenetur ipsam dicta nobis quos non porro!
-            """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Алексей",
-        "date": date(2022, 6, 11),
-        "title": "Программировать интересно!",
-        "excerpt": "Вы когда-нибудь тратили часы на поиск одной мелкой ошбки в коде? Да, именно это и случилось со мной вчера...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Алексей",
-        "date": date(2020, 8, 5),
-        "title": "Природа во всей красе!",
-        "excerpt": "Природа удивительна! Вдохновение, которое я получаю от прогулок на свежем воздухе удивительно.",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        'slug' : 'mountain-hike',
-        'image' : 'mountains.jpg',
-        'author' : "Алексей",
-        'date' : date(2019, 7, 12),
-        'title' : 'mountain hiking',
-        'excerpt' : 'Нет ничего похожего на пейзажи, которые можно увидеть путешествуя по горам! Я не был готов к тому, что случилось, пока я наслаждался прекрасным видом...',
-        'content' : """
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias deleniti, error 
-            ipsum dolorem tempora voluptas repellendus eum corrupti aperiam sit suscipit voluptatem doloremque, 
-            tenetur ipsam dicta nobis quos non porro!
-
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias deleniti, error 
-            ipsum dolorem tempora voluptas repellendus eum corrupti aperiam sit suscipit voluptatem doloremque, 
-            tenetur ipsam dicta nobis quos non porro!
-
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias deleniti, error 
-            ipsum dolorem tempora voluptas repellendus eum corrupti aperiam sit suscipit voluptatem doloremque, 
-            tenetur ipsam dicta nobis quos non porro!
-            """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Алексей",
-        "date": date(2019, 6, 11),
-        "title": "Программировать интересно!",
-        "excerpt": "Вы когда-нибудь тратили часы на поиск одной мелкой ошбки в коде? Да, именно это и случилось со мной вчера...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Алексей",
-        "date": date(2019, 8, 5),
-        "title": "Природа во всей красе!",
-        "excerpt": "Природа удивительна! Вдохновение, которое я получаю от прогулок на свежем воздухе удивительно.",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
 def get_date(post):
     return post['date']
 
 
 def index(request):
-    sorted_posts = sorted(postlist, key=get_date)
-    latest_posts = sorted_posts[-3:]
     return render(request, "blog/index.html", {
         'posts' : latest_posts
     })
@@ -146,12 +21,13 @@ def index(request):
 
 def blog(request):
     return render(request, "blog/posts.html", {
-        'blog' : postlist
+        'blog' : all_posts
     })
 
 
 def post(request, name):
-    article = next(post for post in postlist if post['slug'] == name)
+    article = get_object_or_404(Post, slug=name)
     return render(request, "blog/article.html", {
-        'article' : article
+        'article' : article,
+        'tags' : article.tag.all()
     })
