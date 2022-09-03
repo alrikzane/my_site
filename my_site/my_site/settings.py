cfc2021da5042af648911d0c10deecbde69d8708
alrikzane <ckill071@gmail.com>
2022-09-03 13:02:59 +0300

    debug on

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s#=k7_yrggu=ebhmz+s5eub3@lp-s2yk$=f7itw$7kteexs66e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
